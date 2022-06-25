b427c1d30e30c25ebd6e746d8901a5896c9722d7
Felipe <felipecss88@gmail.com>
2022-06-25 20:03:16 -0300

    Logo svg aplicado

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -1,9 +1,12 @@
 import styles from './Header.module.css'
+import RocketLogo from '../Assets/Rocket2.svg'
 
 export function Header(){
 
     return(
         <header className={styles.Header}>
+          
+          <img src={RocketLogo} alt="" />
           <strong className={styles.to}>to</strong>
           <strong className={styles.do}>do</strong>
 

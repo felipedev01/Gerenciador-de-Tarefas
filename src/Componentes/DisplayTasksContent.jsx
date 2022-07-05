f463e94c5c5cc7aec47e3bd2d57459b555d8dabb
Felipe <felipecss88@gmail.com>
2022-07-05 18:07:00 -0300

    renderização condicional aplicada

--- src/Componentes/DisplayTasksContent.jsx
+++ src/Componentes/DisplayTasksContent.jsx
@@ -6,18 +6,42 @@ import ClipBoard from '../Assets/Clipboard.png'
 export function DisplayTasksContent(props){
 
   console.log(props.tasksContent)
+   
+  if(props.tasksContent.length == 0){
 
     return(
-     <div>
-      <div className={styles.taskContainerIcon}>
-     <img src={ClipBoard} className={styles.emptyIcon}></img>
+      <div>
+       <div className={styles.taskContainerIcon}>
+      <img src={ClipBoard} className={styles.emptyIcon}></img>
+      </div>
+       <div className={styles.taskContainetText}>
+       <p className={styles.paragraph1}>Você ainda não tem tarefas cadastradas</p>
+       <p>Crie tarefas e organize seus itens a fazer</p>
      </div>
-      <div className={styles.taskContainetText}>
-      <p className={styles.paragraph1}>Você ainda não tem tarefas cadastradas</p>
-      <p>Crie tarefas e organize seus itens a fazer</p>
-    </div>
+      </div>
+     
+     )
+
+
+  }else{
+
+    return(
+      <div>
+       <div className={styles.taskContainerIcon}>
+      <img src={ClipBoard} className={styles.emptyIcon}></img>
+      </div>
+       <div className={styles.taskContainetText}>
+       <p className={styles.paragraph1}>Trocar a caixa de areia do gato',
+    'Alimentar o gato'</p>
+       <p>Crie tarefas e organize seus itens a fazer</p>
      </div>
+      </div>
+     
+     )
+
+
+  }
+
     
-    )
 
 }
